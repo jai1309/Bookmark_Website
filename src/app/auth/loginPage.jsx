5461ea6ed944bfdaa3c57da40509f47240b94fba
Jai Vansh Sharan <jaisharan2@gmail.com>
2026-02-15 08:02:35 +0530

    Updated Deployment URL

--- src/app/auth/loginPage.jsx
+++ src/app/auth/loginPage.jsx
@@ -2,41 +2,37 @@
 
 import { FcGoogle } from "react-icons/fc";
 import { createClient } from "@/lib/supabase/client";
+
 export default function LoginPage() {
   const supabase = createClient();
+
   const loginWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${process.env.PUBLIC_VERCEL_URL}/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
   };
 
   return (
-    <div className="ex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center px-4">
       <div className="w-full max-w-md rounded-xl border border-slate-800 p-6 shadow-lg">
-        {/* Heading */}
-        <div className="flex items-center gap-2 mb-2">
-          <h1 className="text-2xl font-semibold">
-            Marksy | Save smarter, Find faster
-          </h1>
-        </div>
+        <h1 className="text-2xl font-semibold mb-2">
+          Marksy | Save smarter, Find faster
+        </h1>
 
         <p className="text-sm mb-6">
           Sign in to Marksy using Gmail. No password required!
         </p>
 
-        {/* Buttons */}
-        <div className="space-y-3">
-          <button
-            className="w-full flex items-center justify-center gap-2 rounded-md border border-slate-700 py-2.5 text-sm hover:bg-slate-800 transition cursor-pointer"
-            onClick={loginWithGoogle}
-          >
-            <FcGoogle />
-            Google Login
-          </button>
-        </div>
+        <button
+          className="w-full flex items-center justify-center gap-2 rounded-md border border-slate-700 py-2.5 text-sm hover:bg-slate-800 transition"
+          onClick={loginWithGoogle}
+        >
+          <FcGoogle />
+          Google Login
+        </button>
       </div>
     </div>
   );
